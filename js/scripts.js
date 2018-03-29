f0a6d8949d929b9528e2525365de7f90e9f3e6ca
Joy Paglinawan <virliejoyp@yahoo.com>
2018-03-28 20:08:41 -0700

    Started by putting an input area.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+///This project needs  will be able to give a true or false answer, for the question, is this year a leapyear?
+///The year is evenly divisible by 4;
+// If the year can be evenly divided by 100, it is NOT a leap year, unless;
+// The year is also evenly divisible by 400. Then it is a leap year.
+///
